a662980276bdf36a0c15aa2f2369a205337c96f4
Górka Mateusz <maatiug@gmail.com>
2021-12-11 18:04:25 +0100

    clean up and naming convenction

--- js/main.js
+++ js/main.js
@@ -1,115 +1,24 @@
-/**
- * \file JS file of OpenSongController
- * \author Górka Mateusz (kzswieb.eu)
- * \copyright Copyright (c) 2020
- */
 //! Aplication config
 /// IP adress of OpenSong computer
 var IP_COMP = '192.168.8.113:8080';
-///! Interface
+var $currentSlide = undefined;
 $(document).ready(function () {
-    var $currentSlide = undefined; //<br current slide in jq
-    /** Show communicat about error. */
-    function err_display(err) {
-        if (err === void 0) { err = 1; }
-        $('#error' + err).show();
-        $('#welcome').fadeIn(1000);
-    }
-    /** Select a current slide on list.
-     * \param[in] $listCon - jQuery object of #slides-con
-     * \param[in] ffinish - function to do after select.
-     */
-    function update_current($listCon, ffinish) {
-        if ($listCon === void 0) { $listCon = $('#slides-con'); }
-        if (ffinish === void 0) { ffinish = function () { }; }
-        $.get("http://" + IP_COMP + "/presentation/status", function (data, status, xhr) {
-            if (status == "success") {
-                var $xml = $($.parseXML(xhr.responseText));
-                // Current slide
-                if ($currentSlide)
-                    $currentSlide.removeClass('current');
-                $currentSlide = $listCon.find("#slide" + $xml.find('slide').attr('itemnumber'));
-                $currentSlide.addClass('current');
-                // On mode
-                var mode = $xml.find('screen').attr('mode');
-                switch (mode) {
-                    case 'N':
-                        $('#mode').text("");
-                        break;
-                    case 'F':
-                        $('#mode').text("Zamrożony ekran");
-                        break;
-                    case 'L':
-                        $('#mode').text("Wyświetla logo");
-                        break;
-                    case 'H':
-                        $('#mode').text("Tylko tło");
-                        break;
-                    case 'B':
-                        $('#mode').text("Czarny ekran");
-                        break;
-                    case 'W':
-                        $('#mode').text("Biały ekran");
-                        break;
-                    default:
-                        $('#mode').text("Nie znany tryb");
-                        break;
-                }
-                // On finish
-                ffinish();
-            }
-            else {
-                console.error(data, status, xhr);
-                err_display();
-            }
-        }).fail(function () { err_display(2); });
-    }
-    /** Move by vect slide.
-     * \param[in] vect - number of slide, vect>0 -> move down, vect<0 -> move up
-     */
-    function move_slide(vect) {
-        var $moved = $('#slide' + (Number($('.current').find('i').text()) + vect));
-        $moved.addClass('moved');
-        return update_current($('#slides-con'), function () { $moved.removeClass('moved'); });
-    }
-    /** Update list of slides. */
-    function update_list() {
-        $.get("http://" + IP_COMP + "/presentation/slide/list", function (data, status, xhr) {
-            if (status == "success") {
-                var $xml = $($.parseXML(xhr.responseText));
-                var $listCon_1 = $('#slides-con');
-                $listCon_1.empty();
-                $xml.find('response').children().each(function () {
-                    var $row = $('<li></li>');
-                    $row.attr("id", "slide" + $(this).attr('identifier'));
-                    $row.append("<i>" + $(this).attr('identifier') + "</i>");
-                    $row.append("<b>" + $(this).attr('name') + "</b>");
-                    var type = $(this).attr('type');
-                    $row.addClass(type);
-                    $row.append("<a>" + type + "</a>");
-                    $listCon_1.append($row);
-                });
-                update_current($listCon_1);
-            }
-            else {
-                console.error(data, status, xhr);
-                err_display();
-            }
-        }).fail(function () { err_display(3); });
-    }
     /// Default loading list of slides
-    update_list();
+    updateList();
     ////------------------------------------
     /// Welcome panel
     $('#ip-address').val(IP_COMP);
     /// Butt - Welcome
     $('#butt-welcome').click(function () {
         IP_COMP = $('#ip-address').val();
-        update_list();
+        updateList();
         $('#welcome').fadeOut('fast');
     });
     ////------------------------------------
-    $('#slides-con').click(function () { update_list(); update_current(); });
+    $('#slides-con').click(function () {
+        updateList();
+        updateCurrent();
+    });
     ////------------------------------------
     /// Butt - Extendent panel
     $('#butt-ext').click(function () {
@@ -117,34 +26,121 @@ $(document).ready(function () {
     });
     /// Butt - next
     $('#butt-next').click(function () {
-        $.post("http://" + IP_COMP + "/presentation/slide/next", function () { move_slide(1); });
+        $.post("http://" + IP_COMP + "/presentation/slide/next", function () { moveSlide(1); });
     });
     /// Butt - previous
     $('#butt-prev').click(function () {
-        $.post("http://" + IP_COMP + "/presentation/slide/previous", function () { move_slide(-1); });
+        $.post("http://" + IP_COMP + "/presentation/slide/previous", function () { moveSlide(-1); });
     });
     /// Butt - normal mode
     $('.butt-normal').click(function () {
-        $.post("http://" + IP_COMP + "/presentation/screen/normal", function () { update_current(); });
+        $.post("http://" + IP_COMP + "/presentation/screen/normal", function () { updateCurrent(); });
     });
     /// Butt - freez mode
     $('.butt-freeze').click(function () {
-        $.post("http://" + IP_COMP + "/presentation/screen/freeze", function () { update_current(); });
+        $.post("http://" + IP_COMP + "/presentation/screen/freeze", function () { updateCurrent(); });
     });
     /// Butt - black mode
     $('.butt-black').click(function () {
-        $.post("http://" + IP_COMP + "/presentation/screen/black", function () { update_current(); });
+        $.post("http://" + IP_COMP + "/presentation/screen/black", function () { updateCurrent(); });
     });
     /// Butt - white mode
     $('.butt-white').click(function () {
-        $.post("http://" + IP_COMP + "/presentation/screen/white", function () { update_current(); });
+        $.post("http://" + IP_COMP + "/presentation/screen/white", function () { updateCurrent(); });
     });
     /// Butt - background mode
     $('.butt-background').click(function () {
-        $.post("http://" + IP_COMP + "/presentation/screen/hide", function () { update_current(); });
+        $.post("http://" + IP_COMP + "/presentation/screen/hide", function () { updateCurrent(); });
     });
     /// Butt - logo mode
     $('.butt-logo').click(function () {
-        $.post("http://" + IP_COMP + "/presentation/screen/logo", function () { update_current(); });
+        $.post("http://" + IP_COMP + "/presentation/screen/logo", function () { updateCurrent(); });
     });
 });
+/** Show communicat about error. */
+function displayError(err) {
+    if (err === void 0) { err = 1; }
+    $('#error' + err).show();
+    $('#welcome').fadeIn(1000);
+}
+/** Select a current slide on list.
+ * \param[in] $listCon - jQuery object of #slides-con
+ * \param[in] ffinish - function to do after select.
+ */
+function updateCurrent($listCon, onFinish) {
+    if ($listCon === void 0) { $listCon = $('#slides-con'); }
+    if (onFinish === void 0) { onFinish = function () { }; }
+    $.get("http://" + IP_COMP + "/presentation/status", function (data, status, xhr) {
+        if (status == "success") {
+            var $xml = $($.parseXML(xhr.responseText));
+            // Current slide
+            if ($currentSlide)
+                $currentSlide.removeClass('current');
+            $currentSlide = $listCon.find("#slide" + $xml.find('slide').attr('itemnumber'));
+            $currentSlide.addClass('current');
+            // On mode
+            var mode = $xml.find('screen').attr('mode');
+            switch (mode) {
+                case 'N':
+                    $('#mode').text("");
+                    break;
+                case 'F':
+                    $('#mode').text("Zamrożony ekran");
+                    break;
+                case 'L':
+                    $('#mode').text("Wyświetla logo");
+                    break;
+                case 'H':
+                    $('#mode').text("Tylko tło");
+                    break;
+                case 'B':
+                    $('#mode').text("Czarny ekran");
+                    break;
+                case 'W':
+                    $('#mode').text("Biały ekran");
+                    break;
+                default:
+                    $('#mode').text("Nie znany tryb");
+                    break;
+            }
+            onFinish();
+        }
+        else {
+            console.error(data, status, xhr);
+            displayError();
+        }
+    }).fail(function () { displayError(2); });
+}
+/** Move by vect slide.
+ * \param[in] vect - number of slide, vect>0 -> move down, vect<0 -> move up
+ */
+function moveSlide(vect) {
+    var $moved = $('#slide' + (Number($('.current').find('i').text()) + vect));
+    $moved.addClass('moved');
+    return updateCurrent($('#slides-con'), function () { $moved.removeClass('moved'); });
+}
+/** Update list of slides. */
+function updateList() {
+    $.get("http://" + IP_COMP + "/presentation/slide/list", function (data, status, xhr) {
+        if (status == "success") {
+            var $xml = $($.parseXML(xhr.responseText));
+            var $listCon_1 = $('#slides-con');
+            $listCon_1.empty();
+            $xml.find('response').children().each(function () {
+                var $row = $('<li></li>');
+                $row.attr("id", "slide" + $(this).attr('identifier'));
+                $row.append("<i>" + $(this).attr('identifier') + "</i>");
+                $row.append("<b>" + $(this).attr('name') + "</b>");
+                var type = $(this).attr('type');
+                $row.addClass(type);
+                $row.append("<a>" + type + "</a>");
+                $listCon_1.append($row);
+            });
+            updateCurrent($listCon_1);
+        }
+        else {
+            console.error(data, status, xhr);
+            displayError();
+        }
+    }).fail(function () { displayError(3); });
+}
--- ts/main.ts
+++ ts/main.ts
@@ -2,128 +2,29 @@
 /// IP adress of OpenSong computer
 var IP_COMP = '192.168.8.113:8080'
 
+var $currentSlide = undefined
+
 $(document).ready(function () {
-    var $currentSlide = undefined
-
-    /** Show communicat about error. */
-    function err_display(err = 1) {
-        $('#error' + err).show();
-        $('#welcome').fadeIn(1000)
-    }
-
-    /** Select a current slide on list.
-     * \param[in] $listCon - jQuery object of #slides-con
-     * \param[in] ffinish - function to do after select.
-     */
-    function update_current($listCon = $('#slides-con'), ffinish = function () { }) {
-        $.get(
-            "http://" + IP_COMP + "/presentation/status",
-            function (data, status, xhr) {
-                if (status == "success") {
-                    let $xml = $($.parseXML(xhr.responseText))
-
-                    // Current slide
-                    if ($currentSlide) $currentSlide.removeClass('current')
-                    $currentSlide = $listCon.find("#slide" + $xml.find('slide').attr('itemnumber'))
-                    $currentSlide.addClass('current')
-
-                    // On mode
-                    var mode = $xml.find('screen').attr('mode')
-                    switch (mode) {
-                        case 'N':
-                            $('#mode').text("")
-                            break;
-                        case 'F':
-                            $('#mode').text("Zamrożony ekran")
-                            break;
-                        case 'L':
-                            $('#mode').text("Wyświetla logo")
-                            break;
-                        case 'H':
-                            $('#mode').text("Tylko tło")
-                            break;
-                        case 'B':
-                            $('#mode').text("Czarny ekran")
-                            break;
-                        case 'W':
-                            $('#mode').text("Biały ekran")
-                            break;
-                        default:
-                            $('#mode').text("Nie znany tryb")
-                            break;
-                    }
-
-                    ffinish()
-                }
-                else {
-                    console.error(data, status, xhr)
-                    err_display()
-                }
-            }
-        ).fail(function () { err_display(2) })
-    }
-
-    /** Move by vect slide.
-     * \param[in] vect - number of slide, vect>0 -> move down, vect<0 -> move up
-     */
-    function move_slide(vect) {
-        let $moved = $('#slide' + (Number($('.current').find('i').text()) + vect))
-        $moved.addClass('moved')
-        return update_current($('#slides-con'), function () { $moved.removeClass('moved') })
-    }
-
-    /** Update list of slides. */
-    function update_list() {
-        $.get(
-            "http://" + IP_COMP + "/presentation/slide/list",
-            function (data, status, xhr) {
-
-                if (status == "success") {
-                    let $xml = $($.parseXML(xhr.responseText))
-                    let $listCon = $('#slides-con')
-                    $listCon.empty()
-
-                    $xml.find('response').children().each(function () {
-                        let $row = $('<li></li>')
-
-                        $row.attr("id", "slide" + $(this).attr('identifier'))
-                        $row.append("<i>" + $(this).attr('identifier') + "</i>")
-                        $row.append("<b>" + $(this).attr('name') + "</b>")
-
-                        let type = $(this).attr('type')
-                        $row.addClass(type)
-                        $row.append("<a>" + type + "</a>")
-
-                        $listCon.append($row)
-                    })
-
-                    update_current($listCon);
-                }
-                else {
-                    console.error(data, status, xhr)
-                    err_display()
-                }
-            }
-        ).fail(function () { err_display(3) })
-    }
-
     /// Default loading list of slides
-    update_list()
+    updateList()
 
     ////------------------------------------
     /// Welcome panel
     $('#ip-address').val(IP_COMP)
 
     /// Butt - Welcome
-    $('#butt-welcome').click(function () {
+    $('#butt-welcome').click(() => {
         IP_COMP = $('#ip-address').val() as string
 
-        update_list()
+        updateList()
         $('#welcome').fadeOut('fast')
     })
 
     ////------------------------------------
-    $('#slides-con').click(function () { update_list(); update_current(); })
+    $('#slides-con').click(() => {
+        updateList();
+        updateCurrent()
+    })
 
     ////------------------------------------
     /// Butt - Extendent panel
@@ -134,49 +35,151 @@ $(document).ready(function () {
     /// Butt - next
     $('#butt-next').click(function () {
         $.post("http://" + IP_COMP + "/presentation/slide/next",
-            function () { move_slide(1) })
+            function () { moveSlide(1) })
     })
 
     /// Butt - previous
     $('#butt-prev').click(function () {
         $.post("http://" + IP_COMP + "/presentation/slide/previous",
-            function () { move_slide(-1) })
-
+            function () { moveSlide(-1) })
     })
 
     /// Butt - normal mode
     $('.butt-normal').click(function () {
         $.post("http://" + IP_COMP + "/presentation/screen/normal",
-            function () { update_current() })
+            function () { updateCurrent() })
     })
 
     /// Butt - freez mode
     $('.butt-freeze').click(function () {
         $.post("http://" + IP_COMP + "/presentation/screen/freeze",
-            function () { update_current() })
+            function () { updateCurrent() })
     })
 
     /// Butt - black mode
     $('.butt-black').click(function () {
         $.post("http://" + IP_COMP + "/presentation/screen/black",
-            function () { update_current() })
+            function () { updateCurrent() })
     })
 
     /// Butt - white mode
     $('.butt-white').click(function () {
         $.post("http://" + IP_COMP + "/presentation/screen/white",
-            function () { update_current() })
+            function () { updateCurrent() })
     })
 
     /// Butt - background mode
     $('.butt-background').click(function () {
         $.post("http://" + IP_COMP + "/presentation/screen/hide",
-            function () { update_current() })
+            function () { updateCurrent() })
     })
 
     /// Butt - logo mode
     $('.butt-logo').click(function () {
         $.post("http://" + IP_COMP + "/presentation/screen/logo",
-            function () { update_current() })
+            function () { updateCurrent() })
     })
 })
+
+/** Show communicat about error. */
+function displayError(err = 1) {
+    $('#error' + err).show();
+    $('#welcome').fadeIn(1000)
+}
+
+/** Select a current slide on list.
+ * \param[in] $listCon - jQuery object of #slides-con
+ * \param[in] ffinish - function to do after select.
+ */
+function updateCurrent($listCon = $('#slides-con'), onFinish = () => { }) {
+    $.get(
+        "http://" + IP_COMP + "/presentation/status",
+        (data, status, xhr) => {
+            if (status == "success") {
+                let $xml = $($.parseXML(xhr.responseText))
+
+                // Current slide
+                if ($currentSlide)
+                    $currentSlide.removeClass('current')
+                $currentSlide = $listCon.find("#slide" + $xml.find('slide').attr('itemnumber'))
+                $currentSlide.addClass('current')
+
+                // On mode
+                var mode = $xml.find('screen').attr('mode')
+                switch (mode) {
+                    case 'N':
+                        $('#mode').text("")
+                        break
+                    case 'F':
+                        $('#mode').text("Zamrożony ekran")
+                        break
+                    case 'L':
+                        $('#mode').text("Wyświetla logo")
+                        break
+                    case 'H':
+                        $('#mode').text("Tylko tło")
+                        break
+                    case 'B':
+                        $('#mode').text("Czarny ekran")
+                        break
+                    case 'W':
+                        $('#mode').text("Biały ekran")
+                        break
+                    default:
+                        $('#mode').text("Nie znany tryb")
+                        break
+                }
+
+                onFinish()
+            }
+            else {
+                console.error(data, status, xhr)
+                displayError()
+            }
+        }
+    ).fail(() => { displayError(2) })
+}
+
+/** Move by vect slide.
+ * \param[in] vect - number of slide, vect>0 -> move down, vect<0 -> move up
+ */
+function moveSlide(vect) {
+    let $moved = $('#slide' + (Number($('.current').find('i').text()) + vect))
+    $moved.addClass('moved')
+    return updateCurrent($('#slides-con'), () => { $moved.removeClass('moved') })
+}
+
+/** Update list of slides. */
+function updateList() {
+    $.get(
+        "http://" + IP_COMP + "/presentation/slide/list",
+        function (data, status, xhr) {
+
+            if (status == "success") {
+                let $xml = $($.parseXML(xhr.responseText))
+                let $listCon = $('#slides-con')
+                $listCon.empty()
+
+                $xml.find('response').children().each(function () {
+                    let $row = $('<li></li>')
+
+                    $row.attr("id", "slide" + $(this).attr('identifier'))
+                    $row.append("<i>" + $(this).attr('identifier') + "</i>")
+                    $row.append("<b>" + $(this).attr('name') + "</b>")
+
+                    let type = $(this).attr('type')
+                    $row.addClass(type)
+                    $row.append("<a>" + type + "</a>")
+
+                    $listCon.append($row)
+                })
+
+                updateCurrent($listCon);
+            }
+            else {
+                console.error(data, status, xhr)
+                displayError()
+            }
+        }
+    ).fail(function () { displayError(3) })
+}
